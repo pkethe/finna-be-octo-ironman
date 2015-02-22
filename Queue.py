ba6dcbb5dfd6645e44510ebdd5cc54fddf0c776d
pkethe <pranav.kethe@gmail.com>
2015-02-21 20:20:24 -0500

    h1
    changes

--- Queue.py
+++ Queue.py
@@ -1,58 +1,71 @@
 __author__ = 'Pranav'
 
-class Node:
-    def __init__(self):
-        self.value = None
-        self.next = None
-
-    def setNodeValue(self, value, next):
-        self.value = value
-        self.next = next
+from GenericNode import *
+import sys
 
 class Queue:
     def __init__(self):
         self.front = None
         self.rear = None
 
+    # Add elements into the queue
     def enqueue(self, value):
-        temp = Node()
+        temp = GenericNode()
+        temp.value = value
+        temp.next = None
         if self.front == None:
-            temp.setNodeValue(value, None)
             self.front = temp
-            self.rear = temp
+            self.rear = self.front
         else:
-            temp.setNodeValue(value, None)
-            self.rear.next = temp
-            self.rear = temp
+            if self.front == self.rear:
+                self.front.next = temp
+                self.rear = self.front.next
+            else:
+                currentRear = self.rear
+                self.rear = temp
+                currentRear.next = self.rear
 
+    # Remove elements from the queue
     def dequeue(self):
-        tempItem = self.front
         if self.front == None:
-            print "No items to dequeue"
+            return None
         else:
-            tempItem = None
-            self.front = self.front.next
+            if self.front == self.rear:
+                current = self.front
+                self.front = None
+                self.rear = None
+                return current
+            else:
+                current = self.front
+                self.front = self.front.next
+                current.next = None
+                return current
 
-        return tempItem
 
-    def display(self):
-        tempItem = self.front
-        while tempItem != None:
-            print str(tempItem.value) + " ",
-            tempItem = tempItem.next
+    def printItemsInQueue(self):
+        current = self.front
+        while current != None:
+            print str(current.value) + "\t"
+            current = current.next
 
 
 myQueue = Queue()
 
 while True:
-    userInput = int(raw_input("1. Queue 2. Dequeue 3. Display :"))
+    userInput = int(raw_input("1. Queue 2. Dequeue 3. Display 4. Exit :"))
     if userInput == 1:
         value = int(raw_input("Enter number: "))
         myQueue.enqueue(value)
     elif userInput == 2:
-        myQueue.dequeue()
+        item = myQueue.dequeue()
+        if item != None:
+            print "Dequeued " + str(item.value)
+    elif userInput == 3:
+        myQueue.printItemsInQueue()
+    elif userInput == 4:
+        sys.exit()
     else:
-        myQueue.display()
+        print "Invalid option, to mainloop"
 
 
 
