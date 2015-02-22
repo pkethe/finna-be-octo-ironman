f480112f9c61eded693881380215709b9bc298ed
pkethe <pranav.kethe@gmail.com>
2015-02-21 19:29:13 -0500

    Added generic node
    Just simple addition

---
+++ GenericNode.py
@@ -0,0 +1,5 @@
+__author__ = 'Pranav'
+class GenericNode:
+    def __init__(self):
+        self.value = None
+        self.next = None
--- Stack.py
+++ Stack.py
@@ -1,60 +1,76 @@
 __author__ = 'Pranav'
 
-class Node:
-    def __init__(self):
-        self.value = None
-        self.next = None
-
-    def setNodeValue(self, value, next):
-        self.value = value
-        self.next = next
-
+from GenericNode import *
+import sys
 
 class Stack:
 
     def __init__(self):
         self.top = None
 
+    # Remove elements from the stack
     def pop(self):
         if self.top != None:
-            topItem = self.top.value
+            current = self.top
             self.top = self.top.next
-            return topItem
+            current.next = None
+            return current
+        return None
 
+    # Add elements into the Stack
     def push(self, value):
-
+        temp = GenericNode()
+        temp.value = value
+        temp.next = None
         if self.top == None:
-            self.top = Node()
-            self.top.setNodeValue(value,None)
+            self.top = temp
         else:
-            temp = Node()
-            temp.setNodeValue(value,self.top)
+            temp.next = self.top
             self.top = temp
 
-    def display(self):
-        if self.top == None:
-            print "No Nodes"
-        else:
-            temp = self.top
-            while temp != None:
-                print temp.value
-                temp = temp.next
-
+    # print elements into the stack
+    def printStackElements(self):
+        current = self.top
+        while current != None:
+            print current.value
+            current = current.next
 
 """
     ###Main###
 
 """
-print "Hello"
+if __name__ == '__main__':
 
-myStack = Stack()
+    myStack = Stack()
 
-while True:
-    userInput = int(raw_input("1. Push 2. Pop 3. Display :"))
-    if userInput == 1:
-        value = int(raw_input("Enter number: "))
-        myStack.push(value)
-    elif userInput == 2:
-        myStack.pop()
-    else:
-        myStack.display()
+    while True:
+        userInput = raw_input("1. Push 2. Pop 3. Display 4. Exit: ")
+
+        if userInput.isdigit():
+            userInput = int(userInput)
+            if userInput == 1:
+                value = int(raw_input("Enter number: "))
+                myStack.push(value)
+            elif userInput == 2:
+                node = myStack.pop()
+                if node != None:
+                    print str(node.value) + " popped"
+            elif userInput == 3:
+                myStack.printStackElements()
+            elif userInput == 4:
+                sys.exit()
+            else:
+                print "Invalid choice, to main loop"
+
+            def display(self):
+                if self.top == None:
+                    print "No Nodes"
+                else:
+                    temp = self.top
+                    while temp != None:
+                        print temp.value
+                        temp = temp.next
+
+        else:
+            print "Invalid choice, to main loop"
+
